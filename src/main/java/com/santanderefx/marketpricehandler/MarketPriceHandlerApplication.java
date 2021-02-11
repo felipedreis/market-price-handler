d6914262b6bc54e2c5d80a11d21c25140129f1eb
Felipe Duarte <felipeduartedreis@gmail.com>
2021-02-11 18:28:58 -0300

    Added  initial files

---
+++ src/main/java/com/santanderefx/marketpricehandler/MarketPriceHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.santanderefx.marketpricehandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketPriceHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketPriceHandlerApplication.class, args);
+    }
+
+}
